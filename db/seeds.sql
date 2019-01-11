6a93723696fe2fcdf5b375234bd08a8d6f6d5ccf
ant4u <anthonyimontalto@gmail.com>
2019-01-11 01:13:42 -0500

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name)
+VALUES 
+('Bacon Double Cheeseburger'),
+('Big Mac'),
+('Royal with Cheese');
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./config/connection");
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Serve index.handlebars to the root route, populated with all quote data.
+app.get("/", function(req, res) {
+
+});
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
