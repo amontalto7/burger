8aae723c71e4c55e67977a9193cbb6bf837e9791
ant4u <anthonyimontalto@gmail.com>
2019-01-23 19:07:47 -0500

    updated handlebars views to display burgers in appropriate buckets

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,11 +9,11 @@ const burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     burger.selectAll("burgers", function(data) {
-      var hbsObject = {
+      let burgers = {
         burgers: data
       };
     //   console.log(hbsObject);
-      res.render("index", hbsObject);
+      res.render("index", burgers);
     });
   });
   
