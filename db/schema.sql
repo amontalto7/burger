887a9bef6b7405ac61884fbc5037c9be4c6367c7
ant4u <anthonyimontalto@gmail.com>
2019-01-24 18:40:36 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 -- Create the table burgers.
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
+id int AUTO_INCREMENT NOT NULL,
 burger_name varchar(255) NOT NULL,
 devoured boolean default false,
 PRIMARY KEY (id)
